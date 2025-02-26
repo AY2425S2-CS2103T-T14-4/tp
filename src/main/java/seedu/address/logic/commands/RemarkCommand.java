c10a4d2d23913113191b5d4ef65c3df086e9cae9
BladerX11 <BladerX11@proton.me>
2025-02-26 21:32:30 +0800

    Feature: Add command for using Remark

---
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -0,0 +1,88 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Remark;
+
+/**
+ * Changes the remark of an existing person in the address book.
+ */
+public class RemarkCommand extends Command {
+    public static final String COMMAND_WORD = "remark";
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Edits the remark of the person identified "
+            + "by the index number used in the last person listing. "
+            + "Existing remark will be overwritten by the input.\n"
+            + "Parameters: INDEX (must be a positive integer) "
+            + "r/ [REMARK]\n"
+            + "Example: " + COMMAND_WORD + " 1 "
+            + "r/ Likes to swim.";
+    public static final String MESSAGE_ARGUMENTS = "Index: %1$d, Remark: %2$s";
+
+    private final Index index;
+    private final Remark remark;
+
+    /**
+     * @param index  of the person in the filtered person list to edit the remark.
+     * @param remark or the person to be updated to.
+     */
+    public RemarkCommand(Index index, Remark remark) {
+        requireAllNonNull(index, remark);
+
+        this.index = index;
+        this.remark = remark;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        throw new CommandException(String.format(MESSAGE_ARGUMENTS, index.getOneBased(), remark));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((index == null) ? 0 : index.hashCode());
+        result = prime * result + ((remark == null) ? 0 : remark.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null) {
+            return false;
+        }
+
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+
+        RemarkCommand other = (RemarkCommand) obj;
+
+        if (index == null) {
+            if (other.index != null) {
+                return false;
+            }
+        } else if (!index.equals(other.index)) {
+            return false;
+        }
+
+        if (remark == null) {
+            if (other.remark != null) {
+                return false;
+            }
+        } else if (!remark.equals(other.remark)) {
+            return false;
+        }
+
+        return true;
+    }
+
+}
